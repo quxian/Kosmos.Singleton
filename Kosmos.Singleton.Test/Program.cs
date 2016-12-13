8795cd0a8bad188704c68adc2bc0ff1094681381
chn_yongsheng <1062012096@qq.com>
2016-12-13 11:28:08 +0800

    1.添加测试。 2.fix bug

---
+++ Kosmos.Singleton.Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kosmos.Singleton.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3000; i++)
+            {
+                SingleHttpClient.PostException(new { test = "yongsheng", date = DateTime.Now });
+            }
+            Thread.Sleep(TimeSpan.FromSeconds(60));
+        }
+    }
+}
--- Kosmos.Singleton/SingleHttpClient.cs
+++ Kosmos.Singleton/SingleHttpClient.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
 using System.Configuration;
 using System.Net.Http;
 using System.Reflection;
@@ -10,22 +12,16 @@ namespace Kosmos.Singleton
     {
         private static string _logServerAddress = ConfigurationManager.AppSettings["LogServerAddress"];
         private static string _identity = Assembly.GetExecutingAssembly().FullName;
+
         public static HttpClient Client { get; set; }
         static SingleHttpClient()
         {
             Client = new HttpClient();
         }
 
-        public static void PostException<T>(T exception)
+        public static void PostException<V>(V v)
         {
-            try
-            {
-                Task.Run(async () => { await Client.PostAsJsonAsync(_logServerAddress, new { Identity = _identity, Message = exception }); });
-            }
-            catch (Exception e)
-            {
-                Task.Run(async () => { await Client.PostAsJsonAsync(_logServerAddress, new { Identity = _identity, Message = e }); });
-            }
+            Task.Run(async () => await Client.PostAsJsonAsync(_logServerAddress, new { Identity = _identity, Message = JsonConvert.SerializeObject(v) }));
         }
     }
 }
